48bc524199789271f38699c14f72751b1b039599
zhouzhaoping <zhouzhaoping11@gmail.com>
2019-12-24 23:51:38 +0800

    complete chapter4

--- springdemo/src/main/java/com/zhouzhaoping/springdemo/service/LambdaService.java
+++ springdemo/src/main/java/com/zhouzhaoping/springdemo/service/LambdaService.java
@@ -1,17 +1,22 @@
 package com.zhouzhaoping.springdemo.service;
 
 import static java.lang.Character.isDigit;
+import static java.lang.Character.isLowerCase;
 import static java.util.Arrays.asList;
 import static java.util.stream.Collectors.toList;
 
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Optional;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.BinaryOperator;
+import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+import com.zhouzhaoping.springdemo.model.Album;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
@@ -32,6 +37,7 @@ public class LambdaService {
         BinaryOperator<Long> addLongs = (x, y) -> x + y;
         atLeast5.test(4);
         addLongs.apply(1L,2L);
+        return;
     }
 
     /**
@@ -49,7 +55,7 @@ public class LambdaService {
                 .filter(artist -> artist.isFrom("peking"))
                 .count();
 
-        // 常用流操作：collect、map、flatmap、max、min
+        // 常用流操作：collect、map、flatmap（多个stream变成一个stream）；max、min、通用reduce
         List<String> collected = Stream.of("a", "b", "c").collect(Collectors.toList());
         List<String> collected2 = Stream.of("a", "b", "hello").map(string -> string.toUpperCase()).collect(toList());
         List<String> beginningWithNumbers = Stream.of("a", "1abc", "abc1").filter(value -> isDigit(value.charAt(0)))
@@ -62,6 +68,76 @@ public class LambdaService {
         Track shortestTrack = tracks.stream()
                 .min(Comparator.comparing(track -> track.getLength()))
                 .get();
+        int sum = Stream.of(1, 2, 3).reduce(0, (acc, element) -> acc + element);
+        return;
+    }
+
+    private int addUp(Stream<Integer> numbers) {
+        return numbers.reduce(0, Integer::sum);
+    }
+    private List<String> artistAndOrigin(List<Artist> artists) {
+        return artists.stream()
+                .map(artist -> artist.getName() + "+" + artist.getOrigin())
+                .collect(Collectors.toList());
+    }
+    private List<Album> under3songAlbum(List<Album> albums) {
+        return albums.stream()
+                .filter(album -> album.getTracks().size() <= 3)
+                .collect(toList());
+    }
+    private void Exercise3(List<Artist> artists, Album album) {
+        int totalMembers = artists.stream()
+                .map(artist -> artist.getMembers().size())
+                .reduce(0, Integer::sum)
+                .intValue();
+
+        //及早boolean anyMatch(Predicate<? super T> predicate);
+        //惰性Stream<T> limit(long maxSize);
+
+        //高阶boolean anyMatch(Predicate<? super T> predicate);
+        //低阶Stream<T> limit(long maxSize);
+
+        AtomicInteger count = new AtomicInteger(0);
+        List<String> origins = album.getMusicians().forEach(musician -> );
+    }
+    private static long lowercaseCount(String string) {
+        return string.chars().filter(Character::isLowerCase).count();
+    }
+    private Optional<String> lowercaseMaxString(List<String> strings) {
+        return strings.stream()
+                .max(Comparator.comparing(LambdaService::lowercaseCount));
+    }
+    private static <I, O> Stream<O> map(Stream<I> in_stream, Function<I, O> mapper) {
+        return in_stream.reduce(new ArrayList<O>(), (acc, x) -> {
+            List<O> newAcc = new ArrayList<>(acc);
+            newAcc.add(mapper.apply(x));
+            return newAcc;
+        }, (List<O> left, List<O> right) -> {
+            List<O> newLeft = new ArrayList<>(left);
+            newLeft.addAll(right);
+            return newLeft;
+        }).stream();
+    }
+    private static <I> Stream<I> filter(Stream<I> stream, Predicate<I> predicate) {
+        List<I> initial = new ArrayList<>();
+        return stream.reduce(initial,
+                (List<I> acc, I x) -> {
+                    if (predicate.test(x)) {
+                        List<I> newAcc = new ArrayList<>(acc);
+                        newAcc.add(x);
+                        return newAcc;
+                    } else {
+                        return acc;
+                    }
+                },
+                LambdaService::combineLists).stream();
+    }
+
+    private static <I> List<I> combineLists(List<I> left, List<I> right) {
+        // We are copying left to new list to avoid mutating it.
+        List<I> newLeft = new ArrayList<>(left);
+        newLeft.addAll(right);
+        return newLeft;
     }
 
     public void Test() {
